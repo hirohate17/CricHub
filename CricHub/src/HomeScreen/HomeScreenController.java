d48f6edf486b45d0f6726cb9538b9efce53a1711
17hiro <hiroshige.programming17@gmail.com>
2024-08-01 18:38:11 +0900

    vsCodeでローカルでサーバー立てて開発できるように開発環境を構築しなす

--- CricHub/src/HomeScreen/HomeScreenController.java
+++
@@ -1,9 +0,0 @@
-package HomeScreen;
-public class HomeScreenController {
-    /**
-     * HOME画面を描画する
-     */
-    void showHomeScreen(){
-        // HOME画面のHTMLを呼び出す
-    }
-}
---
+++ crichub/src/test/java/com/crichub/crichub/CrichubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crichub.crichub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrichubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
