357c7b718d62e8e9ad9b0a7316fc2f8f623ca5c9
hirohate17 <17hiroshige@gmail.com>
2024-08-08 17:16:13 +0900

    各コントローラーを作成

---
+++ crichub/src/main/java/com/crichub/chat/ChatController.java
@@ -0,0 +1,8 @@
+package com.crichub.chat;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+}
---
+++ crichub/src/main/java/com/crichub/result/ResultController.java
@@ -0,0 +1,8 @@
+package com.crichub.result;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ResultController {
+
+}
