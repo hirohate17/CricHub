eaf1e8c60332b273f0da56ad29013cf3bd38b7fb
hirohate17 <17hiroshige@gmail.com>
2024-08-08 13:30:23 +0900

    unusedなimportを削除

--- crichub/src/main/java/com/crichub/CrichubApplication.java
+++ crichub/src/main/java/com/crichub/CrichubApplication.java
@@ -2,9 +2,6 @@ package com.crichub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.crichub.home.HomeScreenController;
 
 @SpringBootApplication
 public class CrichubApplication {
--- crichub/src/main/java/com/crichub/home/HomeScreenController.java
+++ crichub/src/main/java/com/crichub/home/HomeScreenController.java
@@ -1,13 +1,7 @@
 package com.crichub.home;
 
-import java.awt.Desktop;
-import java.io.File;
-import java.io.IOException;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeScreenController {
